b20fba579158eb8db69da0c9284cabde5266b601
Talhak <talhawkk@gmail.com>
2025-01-01 16:07:13 +0500

    Register Model class using Decorator

--- model/home/admin.py
+++ model/home/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from home.models import Student
 # Register your models here.
+@admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
     list_display=('id','stuid','stuname','stuemail','stupass')
-admin.site.register(Student,StudentAdmin)
+
+# admin.site.register(Student,StudentAdmin)
