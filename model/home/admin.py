280b96465876a73d272c1cc967c0633a26c5229e
Talhak <talhawkk@gmail.com>
2025-01-01 15:59:03 +0500

    Use of ModelAdmin Class and registering

--- model/home/admin.py
+++ model/home/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from home.models import Student
 # Register your models here.
-admin.site.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display=('id','stuid','stuname','stuemail','stupass')
+admin.site.register(Student,StudentAdmin)
